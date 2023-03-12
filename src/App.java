ec27264d2316ea28d6e6400154a77b56ccb6463c
Tonga <118144420+TonguitaXd@users.noreply.github.com>
2023-03-11 23:31:11 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Holaaaaaaaaaaa, tonga");
+        System.out.println("Holaaaaaaaaaaa, tonga xdxd");
     }
 }
